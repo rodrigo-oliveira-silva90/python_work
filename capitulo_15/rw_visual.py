0917d26b92a4f486c206a31420739d7fa22ad483
Rodrigo Oliveira <rdrg.oliveira1@gmail.com>
2023-10-07 17:18:36 -0300

    capitulo_16

---
+++ capitulo_15/rw_visual.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Continua criando novos passeios enquanto o programa estiver ativo
+while True:
+    # Cria um passeio aleatório e plota os pontos
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+    
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+        edgecolor='none', s=1)
+        
+    #Define o tamanho da janela de plotagem
+    #plt.figure(figsize=(10, 6))
+        
+    # Enfatiza o primeiro e o último ponto
+    plt.scatter(0,0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+        s=100)
+        
+    # Remove os eixos
+    #plt.axes().get_xaxis().set_visible(False)
+    #plt.axes().get_yaxis().set_visible(False)
+    
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
---
+++ capitulo_16/high_lows.py
@@ -0,0 +1,29 @@
+import csv
+
+from matplotlib import pyplot as plt
+
+# Obtém as temperaturas máximas do arquivo
+filename = 'sitka_weather_07-2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+        
+    print(highs)   
+    
+# Faz a plotagem dos dados
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(highs, c='red')
+
+# Formata o gráfico
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which="major", labelsize=16)
+
+plt.show()
+    
